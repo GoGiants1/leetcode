3c8ae2ebf38f4a31caba7ecb7fcda6b733c5124f
Hyung Wook <69342392+GoGiants1@users.noreply.github.com>
2023-07-22 22:16:40 +0900

    Time: 92 ms (83.74%) | Memory: 19.9 MB (50.81%) - LeetSync

---
+++ 57-insert-interval/insert-interval.py
@@ -0,0 +1,25 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        out = []
+        curr = newInterval[:]
+
+        for i, itv in enumerate(intervals):
+            is_overlap = False
+            if itv[1] < newInterval[0]:
+                out.append(itv)
+                continue
+            if curr is None:
+                out.append(itv)
+                continue
+            
+            if curr is not None and itv[0] <= curr[0] <= itv[1] or itv[0] <= curr[1] <= itv[1]:
+                curr[0] = min(itv[0], curr[0])
+                curr[1] = max(itv[1], curr[1])
+            
+            elif curr[1] < itv[0]:
+                out.append(curr)
+                curr = None
+                out.append(itv)
+        if curr:
+            out.append(curr)
+        return out
