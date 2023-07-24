4acf6c3924964015f43d6b66a052a924d3bdf187
Hyung Wook <69342392+GoGiants1@users.noreply.github.com>
2023-07-24 14:17:58 +0900

    Time: 127 ms (18.68%) | Memory: 17.7 MB (90.33%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,33 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0 for _ in range(n)]
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xset = self.find(x)
+        yset = self.find(y)
+        if xset == yset:
+            return
+        if self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = self.parent[xset]
+        elif self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = self.parent[yset]
+        else:
+            self.parent[xset] = self.parent[yset]
+            self.rank[yset] += 1
+            
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        def bfs(s: int):
-            q = deque()
-            q.append(s)
-            while q:
-                node = q.popleft()
-                visit.add(node)
-
-                for w in g[node]:
-                    if w not in visit:
-                        q.append(w)
+        ds = DSU(n)
+        for edge in edges:
+            ds.union(edge[0], edge[1])
         
-        # do bfs for every node
-        g = defaultdict(list)
-        for k, v in edges:
-            g[k].append(v)
-            g[v].append(k)
-
-        visit = set()
-        cnt = 0
-        for start in range(n):
-            if start not in visit:
-                bfs(start)
-                cnt += 1
-
-        
-        
-
-                
-        return cnt
+        parent = set()
+        for i in range(n):
+            parent.add(ds.find(i))
+        return len(parent)
