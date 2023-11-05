1cf06540a2fbd122a5b03bc9abc96d9436dcea6e
Hyung Wook <69342392+GoGiants1@users.noreply.github.com>
2023-11-06 00:11:25 +0900

    Time: 532 ms (59.78%) | Memory: 30 MB (63.04%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.py
@@ -1,26 +1,20 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         # k번 연속으로 이기면 종료
-        if len(arr) < k:
-            return max(arr)
-
-        q = deque(arr)
-        winner = None
-        loser = None
+        curr = arr[0]
+        max_el = max(arr)
         win_cnt = 0
-
-
-        while win_cnt < k:
-            prev_winner = winner
-            a = q.popleft()
-            b = q.popleft()
-            winner, loser = max(a,b), min(a,b)
-            q.appendleft(winner)
-            q.append(loser)
-            if prev_winner == winner:
+        for i in range(1, len(arr)):
+            opponent = arr[i]
+            if curr > opponent:
                 win_cnt += 1
             else:
-                prev_winner = winner
                 win_cnt = 1
+                curr = opponent
+                
+            if win_cnt == k:
+                return curr
+            if curr == max_el:
+                return curr
 
-        return winner
+            
