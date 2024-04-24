024eac94621371f4b4d8985a02575a5d23eb35ec
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-04-24 15:18:26 +0900

    Time: 11 ms (37.77%) | Memory: 9.9 MB (5.92%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,8 +7,10 @@ public:
         if (n != m) {
             return false;
         }
-        map<char, int> mp;
-
+        // map<char, int> mp; bottle neck 지점 -> 그냥 맵은 search가 O(logn)
+        // 따라서 unorderd_map이나 set을 써야함.
+        unordered_map<char, int> mp; // O(1)로 원소 접근 가능.
+        
         // O(m)
         for (int i = 0; i < m; ++i){
             ++mp[s[i]];
