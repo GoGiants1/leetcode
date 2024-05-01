3ebb6c927108c4866bd257225950fde8c4fc0cc8
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-05-01 15:53:17 +0900

    Time: 3 ms (92.21%) | Memory: 27.1 MB (73.36%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -4,14 +4,18 @@ public:
         // even length (include 0 len)
         // A pop, B pop from nums -> B push, A push to arr
         // 풀이법: sort.
-        vector<int> arr;
         std::sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size(); i += 2){
-            arr.push_back(nums[i+1]);
-            arr.push_back(nums[i]);
-        }
+        // vector<int> arr;
+        // for(int i = 0; i < nums.size(); i += 2){
+        //     arr.push_back(nums[i+1]);
+        //     arr.push_back(nums[i]);
+        // }
 
-        return arr;
+        // return arr;
+        for(int i = 0; i < nums.size(); i += 2){
+            std::swap(nums[i], nums[i+1]);
+        }
+        return nums;
     }
 };
