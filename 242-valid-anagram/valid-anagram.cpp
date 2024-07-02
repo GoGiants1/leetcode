498b7e4ba9043118e38ea48c7d679a824ff83c79
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-07-02 15:47:06 +0900

    Time: 3 ms (97.06%) | Memory: 9.8 MB (5.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // check char freq!
-        int m = s.size();
-        int n = t.size();
-        if (n != m) {
+        int m, n;
+        m = s.size();
+        n = t.size();
+        
+        if (m != n){
             return false;
         }
-        // map<char, int> mp; bottle neck 지점 -> 그냥 맵은 search가 O(logn)
-        // 따라서 unorderd_map이나 set을 써야함.
-        unordered_map<char, int> mp; // O(1)로 원소 접근 가능.
-        
-        // O(m)
-        for (int i = 0; i < m; ++i){
-            ++mp[s[i]];
-            --mp[t[i]];
+
+        unordered_map<char, int> cnt_1;
+        unordered_map<char, int> cnt_2;
+
+
+        // O(N)
+        for (int i=0; i < m; ++i){
+            cnt_1[s[i]] += 1;
+            cnt_2[t[i]] += 1;
         }
-        // O(k), k is number of keys in each map
-        for (auto a: mp){
-            if (a.second != 0){
+
+        for (auto el: cnt_1){
+            char c;
+            int count;
+            tie(c, count) = el;
+            if (cnt_2.count(c) > 0){
+                if(cnt_2[c] != count){
+                    return false;
+                }
+            }
+            else{
                 return false;
             }
         }
