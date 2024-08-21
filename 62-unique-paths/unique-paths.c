1442fe064da5d282fcb52cc1774454753bf3f576
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-08-21 18:58:49 +0900

    Time: 3 ms (43.10%) | Memory: 7.6 MB (47.14%) - LeetSync

--- 62-unique-paths/unique-paths.c
+++ 62-unique-paths/unique-paths.c
@@ -1,5 +1,6 @@
+int memo[101][101];
 
-int dp(int i, int j, int **memo) {
+int dp(int i, int j) {
     if (i == 0 || j == 0) {
         return 1;
     }
@@ -8,23 +9,19 @@ int dp(int i, int j, int **memo) {
         return memo[i][j];
     }
 
-    memo[i][j] = dp(i - 1, j, memo) + dp(i, j - 1, memo);
+    memo[i][j] = dp(i - 1, j) + dp(i, j - 1);
     return memo[i][j];
 }
 
 int uniquePaths(int m, int n) {
-    int **memo = (int **)malloc(m * sizeof(int *));
-    for (int i = 0; i < m; i++) {
-        memo[i] = (int *)calloc(n, sizeof(int));
+    for (int i = 0; i < m; i++){
+        memo[i][0] = 0;
+    }
+    for (int j = 0; j < n; j++){
+        memo[0][j] = 0;
     }
 
-    int result = dp(m - 1, n - 1, memo);
-
-    // 메모리 해제
-    for (int i = 0; i < m; i++) {
-        free(memo[i]);
-    }
-    free(memo);
+    int result = dp(m - 1, n - 1);
 
     return result;
 }
