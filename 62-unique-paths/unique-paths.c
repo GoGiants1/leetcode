6792e9d34dd22a1fb9b87b8deedd662ba537420f
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-08-21 17:27:03 +0900

    Time: 3 ms (43.10%) | Memory: 7.7 MB (35.48%) - LeetSync

--- 62-unique-paths/unique-paths.c
+++ 62-unique-paths/unique-paths.c
@@ -1,23 +1,20 @@
 int uniquePaths(int m, int n) {
     int grid[m][n];
 
+    // 중학교 수학 문제
+
     for(int i = 0; i < m; ++i){
-        for (int j = 0; j < n; ++j){
-            grid[i][j] = 0;
-        }
+        grid[i][0] = 1;    
     }
-    grid[0][0] = 1;
+    for (int j = 0; j < n; ++j){
+        grid[0][j] = 1;
+    }
 
     // The test cases are generated so that the answer will be less than or equal to 2 * 10^9 = 2 * (2^10)^3 = 2^31. -> int32 보장
 
-    for (int i = 0; i < m; ++i){
-        for (int j = 0; j < n; ++j){
-            if (i > 0){
-                grid[i][j] += grid[i-1][j];
-            }
-            if (j > 0){
-                grid[i][j] += grid[i][j-1];
-            }
+    for (int i = 1; i < m; ++i){
+        for (int j = 1; j < n; ++j){
+            grid[i][j] = grid[i-1][j] + grid[i][j-1];
         }
     }
 
