63417cf6203780a1a05a2821739d65318ed3ac72
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2024-08-22 22:11:06 +0900

    Time: 822 ms (64.72%) | Memory: 33 MB (76.58%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        
+        # 길이가 1인 부분 순서는 모두 팰린드롬
+        for i in range(n):
+            dp[i][i] = 1
+        
+        # 길이가 2 이상인 부분 순서에 대해 계산
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        
+        return dp[0][n-1]
