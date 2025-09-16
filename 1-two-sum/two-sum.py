133191d0942a2514e5f77f0f62f4693995d6f5d2
Hyungwook Choi <69342392+GoGiants1@users.noreply.github.com>
2025-09-17 01:25:07 +0900

    Time: 0 ms (100.00%) | Memory: 19.1 MB (10.25%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,18 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # map
-        from collections import defaultdict       
-        nums_dict = defaultdict(list)
-
+        # sol 1: Time: O(n), Space: O(n)
+        d = {}
         for i, v in enumerate(nums):
-            nums_dict[v].append(i)
-
-        for k in nums_dict.keys():
-            pair = target - k
-            if pair in nums_dict:
-                if pair != k:
-                    return [nums_dict[k][0], nums_dict[pair][0]]
-                else:
-                    if len(nums_dict[k]) >= 2:
-                        return nums_dict[k][:2]
-        return []            
+            need = target - v
+            if need in d:
+                return [d[need], i]
+            
+            d[v] = i
+            
